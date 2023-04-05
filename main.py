c1971e4d1444f7bbb3bee2072d00a65ff41c562f
jlabadorf <james@jameslabadorf.com>
2023-04-04 20:15:50 -0400

    Added methods.... oops

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/remove')
+@app.route('/remove', methods=['POST','GET'])
 def remove():
     if request.method == 'POST':
         text = ""
